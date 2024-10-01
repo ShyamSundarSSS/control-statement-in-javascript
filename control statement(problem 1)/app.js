91d4fe75084d7431450fb9afe3e1eab70c3cf91b
Shyam Sundar S <161578766+ShyamSundarSSS@users.noreply.github.com>
2024-10-01 14:04:47 +0530

    Add files via upload

---
+++ control statement(problem 1)/app.js
@@ -0,0 +1,29 @@
+// let firstname ="james";
+// let secondname ="smith";
+// let fullname=firstname+secondname;
+// fullname+="williams"
+// console.log(fullname);
+// console.log("my first name is "+firstname+" and my second name is "+secondname);
+// const fruits=['apple','orange','banana'];
+// const mixedArr=['one',['two',2],3,4];
+// console.log(mixedArr);
+// console.log(mixedArr[1][1])
+
+
+
+//if else
+let m=prompt("enter a number")
+let n=prompt("enter a another number")
+sum=0;
+if(m>n){
+    console.log("ZERO")
+}
+else{
+    for(i=m;i<=n;i++){
+
+        sum+=i*i*i;
+    }
+
+
+console.log(sum)
+}
